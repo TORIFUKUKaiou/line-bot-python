711ca2c5008fbefae40ce4f4e44660c8e6149f65
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2023-06-09 09:51:03 +0900

    こんにちは

--- app.py
+++ app.py
@@ -37,9 +37,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if 'こんにちは' in event.message.text:
+        reply = 'Hello, world!'
+    else:
+        reply = event.message.text
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=reply))
 
 
 if __name__ == "__main__":
