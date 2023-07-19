50d9b7f4351a474f57ede5f414b1fa1cc9269acd
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2023-07-19 09:58:31 +0900

    refactor

--- talkapi.py
+++ talkapi.py
@@ -9,9 +9,11 @@ def call(query_msg):
     response = requests.post('https://api.a3rt.recruit.co.jp/talk/v1/smalltalk', files=files)
     json = response.json()
     pprint.pprint(json)
-    print(json['results'][0]['reply'])
 
-    return json['results'][0]['reply']
+    reply = json['results'][0]['reply']
+    print(reply)
+
+    return reply
 
 if __name__ == "__main__":
     call('好きな食べ物は何ですか')
