c29618752b2457a0bb6036812f2cf8b0c7b904b1
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2024-08-26 17:52:35 +0900

    update dogapi.py

--- dogapi.py
+++ dogapi.py
@@ -1,10 +1,10 @@
 import requests
 
-def get_items(query):
+def get_image():
     response = requests.get(f'https://dog.ceo/api/breeds/image/random')
     print(response)
     print(response.json())
 
 
 if __name__ == "__main__":
-    get_items('Python')
+    get_image()
